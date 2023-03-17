07d89a307854ddaad91771d19b714c25c1025e23
jinftw64 <jin@PLATCUBAHILL16>
2023-03-17 11:17:59 -0400

    2nd commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('.container');
+
+function createSquare() {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  return square;
+}
+
+for ()
