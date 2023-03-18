f6f577634d2bca1ee9daf6e614212909e1291005
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2023-03-17 22:22:56 -0400

    Implement etch-a-sketch that prompts user for size of grid side

--- scripts.js
+++ scripts.js
@@ -1,16 +1,49 @@
-const container = document.querySelector('.container');
-
 function createSquare() {
   const square = document.createElement('div');
   square.classList.add('square');
   return square;
 }
 
-for (let i = 0; i <= 15; i ++) {
-  container.appendChild(createSquare());
+function createManySquares(num) {
+  const container = document.querySelector('.container');
+  for (let i = 0; i <= (num ** 2); i ++) {
+    container.appendChild(createSquare());
+  };
+};
+
+function addColorClass() {
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(square => square.addEventListener("mouseenter", function () {
+    square.classList.add('color');
+  }));
+};
+
+function setGridColumnsRows(num) {
+  const container = document.querySelector('.container');
+  container.setAttribute('style', `grid-template-columns: repeat(${num}, 1fr); grid-template-rows: repeat(${num}, 1fr);`);
 }
 
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => square.addEventListener("mouseenter", function () {
-  square.classList.add('color');
-}));
+function askSizeofSide() {
+  let numOfSides = 0;
+  while (numOfSides <= 0 || numOfSides > 100) {
+    numOfSides = parseInt(prompt('Enter size of square side:'));
+  }
+  return numOfSides;
+}
+
+function removeEveryChild() {
+  const container = document.querySelector('.container');
+  while (container.firstChild) {
+    container.removeChild((container.lastChild));
+  }
+};
+
+const container = document.querySelector('.container');
+const button = document.querySelector('button');
+button.addEventListener('click', function() {
+  let numOfSides = askSizeofSide();
+  setGridColumnsRows(numOfSides);
+  removeEveryChild();
+  createManySquares(numOfSides);
+  addColorClass();
+});
